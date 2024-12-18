9d3a48a471ac689e06e41e9f922feab39e8b1328
shashankjn001 <shashankjn111@gmail.com>
2024-12-18 23:56:23 +0530

    initial commit

---
+++ src/main/java/com/shashank/JavaDevelopmentLearning/JavaDevelopmentLearningApplication.java
@@ -0,0 +1,13 @@
+package com.shashank.JavaDevelopmentLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevelopmentLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevelopmentLearningApplication.class, args);
+	}
+
+}
